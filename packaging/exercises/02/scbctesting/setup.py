def4267735bf64d9bf77e83781bdd417db25d3ab
Olivier Grisel <olivier.grisel@ensta.org>
2013-04-21 10:53:28 +0200

    Started work on introduction to packaging

---
+++ packaging/exercises/02/scbctesting/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="scbctesting",
+    version="0.1.0",
+    description="Sample project for the Software Carpentry BootCamp",
+    author="Firstname Lastname",
+    author_email="contact@example.com",
+    license='BSD',
+    url='http://github.com/ogrisel/scbctesting',
+    packages=['scbctesting'],
+)
